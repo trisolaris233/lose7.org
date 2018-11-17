6b60497cf8c929db7429e461934d7eb5234b4de2
trisolaris233 <zhanshugegpu@163.com>
2018-11-17 00:00:02 -0500

    2018/11/17 00:00:01

--- usr/themes/Lose7/footer.php
+++ usr/themes/Lose7/footer.php
@@ -6,7 +6,7 @@
 
 <footer id="footer" role="contentinfo">
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>.
-    <?php _e('由 <a href="http://www.typecho.org">Typecho</a> 强力驱动'); ?>.
+    <?php _e('Powered by <a href="http://www.typecho.org">Typecho</a> | Theme designed by <a href="https://lose7.org/about.html">Sunshine+Ice</a>'); ?>.
 </footer><!-- end #footer -->
 
 <?php $this->footer(); ?>
